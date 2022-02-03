124140c71046510df56574160a016a1e10419a22
Ludovic Allegaert <ludovic.allegaert@axa.fr>
2022-02-03 15:45:04 +0100

    initial commit

---
+++ src/main/java/com/ludo/msnote/MsnoteApplication.java
@@ -0,0 +1,13 @@
+package com.ludo.msnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsnoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsnoteApplication.class, args);
+    }
+
+}
